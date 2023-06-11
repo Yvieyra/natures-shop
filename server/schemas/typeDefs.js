9253e2961be612b4cf6bb1ccf8890966d5be4d0c
Ying Yang <vieyrayessenia8@gmail.com>
2023-06-10 22:57:04 -0600

    adding User typeDegs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,13 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+type User {
+    _id: id
+    firstName: String
+    lastName: String
+    email: String
+    orders: {Order}
+}
+`;
+
+module.exports = typeDefs;
