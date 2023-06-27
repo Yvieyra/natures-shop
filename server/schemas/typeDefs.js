4c00ab320f7651437ac583e3bcf448c65da6b5c0
Ying Yang <vieyrayessenia8@gmail.com>
2023-06-26 22:06:38 -0600

    adding types to server typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,7 +7,47 @@ type User {
     lastName: String
     email: String
     orders: {Order}
+},
+type Product {
+_id: id
+name: String
+description: String
+image: String
+quantity: Int
+price: Float
+category: Category
+},
+ type Order {
+    _id: id
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+ }, 
+ type Checkout {
+    session: ID
+ },
+ type Auth {
+    token: ID
+    user: User
+ },
+ type Category {
+    _id: ID
+    name: String
+ },
+ type Query {
+categories: [Category]
+products(category: ID, name: String): [Product]
+product(_id:ID!): Product
+user: User
+order(_id:ID!): Order
+checkout: (products: [ID]!): Checkout
+ },
+
+type Mutation {
+    addUser(firstName: String!, lastName: String!, )
 }
+ }
 `;
 
 module.exports = typeDefs;
